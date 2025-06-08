3c8196b149c077f511056d3c47b32886032f9ce9
Vichu <javierlopezranero@gmail.com>
2025-06-08 19:28:38 +0200

    Primer commit

---
+++ Conversor/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Conversor;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Conversor/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Conversor;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
